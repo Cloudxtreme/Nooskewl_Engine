7b9bc0fa423dff29dbe9092ec3b5f9ca011030ab
Trent Gamblin <trent@nooskewl.ca>
2015-07-01 16:11:29 -0600

    Added basic XML parser

---
+++ include/xml.h
@@ -0,0 +1,38 @@
+#ifndef XML_H
+#define XML_H
+
+#include "starsquatters.h"
+
+class XML {
+public:
+	XML(std::string name, std::string value);
+	XML(std::string filename);
+	~XML();
+
+	std::list<XML *> &get_nodes();
+	XML *find(std::string name);
+	std::string get_value();
+	void add(XML *node);
+	std::string get_name();
+	bool failed();
+
+protected:
+	XML(std::string name, SDL_RWops *f);
+
+	int get();
+	void unget(int c);
+	void read();
+	std::string read_token();
+	std::string get_token_name(std::string token);
+	void write(SDL_RWops *out, int tab);
+
+	SDL_RWops *file;
+	std::string name;
+	std::string value;
+	std::list<XML *> nodes;
+	bool debug;
+	int ungot;
+	bool ungotReady;
+};
+
+#endif // XML_H
