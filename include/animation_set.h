818cb60d5999a46673b95f015d2b8e1456ddd568
Trent Gamblin <trent@nooskewl.ca>
2015-07-01 16:11:58 -0600

    Added Animation_Set class (untested)

---
+++ include/animation_set.h
@@ -0,0 +1,39 @@
+#ifndef ANIMATION_SET_H
+#define ANIMATION_SET_H
+
+#include "starsquatters.h"
+#include "image.h"
+
+class Animation_Set {
+public:
+	Animation_Set();
+	~Animation_Set();
+
+	bool load(std::string xml_filename, std::string image_directory);
+
+	bool set_animation(std::string name);
+
+	void start();
+	void stop();
+	// set to frame 0
+	void reset();
+
+	Image *get_current_image();
+
+private:
+	struct Animation {
+		std::vector<Image *> images;
+		std::vector<Uint32> delays;
+		Uint32 total_delays;
+	};
+
+	bool started;
+	Uint32 start_time;
+
+	// "" when not set
+	std::string current_animation;
+
+	std::map<std::string, Animation *> animations;
+};
+
+#endif // ANIMATION_SET_H
