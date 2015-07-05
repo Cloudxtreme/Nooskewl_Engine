f1bb1f0b5d5206e1ee16d7fedac160ff2381a767
Trent Gamblin <trent@nooskewl.ca>
2015-07-04 20:29:57 -0600

    Load a palette

--- include/graphics.h
+++ include/graphics.h
@@ -4,7 +4,12 @@
 bool init_graphics();
 void shutdown_graphics();
 
+bool load_palette(std::string name);
+void draw_quad(float x, float y, float w, float h, SDL_Colour vertex_colours[4]);
 void draw_quad(float x, float y, float w, float h, SDL_Colour colour);
 void draw_window(float x, float y, float w, float h);
+
+extern SDL_Colour colours[256];
+extern SDL_Colour four_whites[4];
 
 #endif // GRAPHICS_H
--- src/graphics.cpp
+++ src/graphics.cpp
@@ -1,14 +1,26 @@
 #include "starsquatters.h"
 #include "graphics.h"
 #include "image.h"
+#include "log.h"
+#include "util.h"
 #include "vertex_accel.h"
 
 static Image *window_image;
+
+SDL_Colour colours[256];
+SDL_Colour four_whites[4];
 
 bool init_graphics()
 {
 	window_image = new Image();
 	if (window_image->load_tga("misc_graphics/window.tga") == false) {
+		errormsg("Error loading misc_graphics/window.tga!\n");
+		delete window_image;
+		return false;
+	}
+
+	if (load_palette("nes.gpl") == false) {
+		errormsg("Couldn't load nes.gpl!\n");
 		delete window_image;
 		return false;
 	}
@@ -21,31 +33,89 @@ void shutdown_graphics()
 	delete window_image;
 }
 
-void draw_quad(float x, float y, float w, float h, SDL_Colour colour)
+bool load_palette(std::string name)
+{
+	SDL_RWops *file = open_file(name);
+	if (file == NULL) {
+		errormsg("Couldn't load palette: %s\n", name.c_str());
+		return false;
+	}
+
+	char line[1000];
+
+	SDL_fgets(file, line, 1000);
+	if (strncmp(line, "GIMP Palette", 12)) {
+		errormsg("Not a GIMP palette (%s)\n", name.c_str());
+		SDL_RWclose(file);
+		return false;
+	}
+
+	int line_count = 1;
+	int colour_count = 0;
+
+	while (SDL_fgets(file, line, 1000) != NULL) {
+		line_count++;
+		char *p = line;
+		while (*p != 0 && isspace(*p)) p++;
+		// Skip comments
+		if (*p == '#') {
+			continue;
+		}
+		int r, g, b;
+		if (sscanf(line, "%d %d %d", &r, &g, &b) == 3) {
+			colours[colour_count].r = r;
+			colours[colour_count].g = g;
+			colours[colour_count].b = b;
+			colours[colour_count].a = 255;
+			colour_count++;
+		}
+		else {
+			infomsg("Syntax error on line %d of %s\n", line_count, name.c_str());
+		}
+	}
+
+	for (int i = 0; i < 4; i++) {
+		four_whites[i].r = 255;
+		four_whites[i].g = 255;
+		four_whites[i].b = 255;
+		four_whites[i].a = 255;
+	}
+
+	SDL_RWclose(file);
+
+	return true;
+}
+
+void draw_quad(float x, float y, float w, float h, SDL_Colour vertex_colours[4])
 {
 	glBindTexture(GL_TEXTURE_2D, 0);
 	vertex_accel->start();
-	vertex_accel->buffer(0, 0, 0, 0, x, y, w, h, colour.r/255.0f, colour.g/255.0f, colour.b/255.0f, colour.a/255.0f, 0);
+	vertex_accel->buffer(0, 0, 0, 0, x, y, w, h, vertex_colours, 0);
 	vertex_accel->end();
+}
+
+void draw_quad(float x, float y, float w, float h, SDL_Colour colour)
+{
+	static SDL_Colour vertex_colours[4];
+	for (int i = 0; i < 4; i++) {
+		vertex_colours[i] = colour;
+	}
+	draw_quad(x, y, w, h, vertex_colours);
 }
 
 void draw_window(float x, float y, float w, float h)
 {
-	// FIXME: use palette
-	SDL_Colour colours[4] = {
-		{ 162, 186, 255, 220 },
-		{ 81, 30, 255, 220 },
-		{ 65, 65, 255, 220 },
-		{ 40, 0, 186, 220 }
-	};
+	SDL_Colour vertex_colours[4];
 
-	float bg_w = w - 2;
-	float bg_h = h - 2;
-	float section_h = bg_h / 4;
+	// Blue shades in NES palette
+	vertex_colours[0] = vertex_colours[1] = colours[47];
+	vertex_colours[2] = vertex_colours[3] = colours[44];
 
 	for (int i = 0; i < 4; i++) {
-		draw_quad(x+1, y+1+i*section_h, bg_w, section_h, colours[i]);
+		vertex_colours[i].a = 220;
 	}
+
+	draw_quad(x+1, y+1, w-2, h-2, vertex_colours);
 
 	window_image->start();
 	window_image->draw_region(0, 0, 6, 6, x, y, 0); // top left
