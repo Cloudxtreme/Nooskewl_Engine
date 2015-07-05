d76aa9ba6556ce32e432712fcb29ea24b08b5093
Trent Gamblin <trent@nooskewl.ca>
2015-07-04 23:54:50 -0600

    Use a timer

--- src/main.cpp
+++ src/main.cpp
@@ -7,14 +7,15 @@
 #include "player_brain.h"
 #include "video.h"
 
+Map *map;
+Map_Entity *player;
+
 bool run_main();
 
 int main(int argc, char **argv)
 {
 	try {
-		if (run_main() == false) {
-			return 1;
-		}
+		run_main();
 	}
 	catch (Error e) {
 		errormsg("Fatal error: %s\n", e.error_message.c_str());
@@ -24,10 +25,25 @@ int main(int argc, char **argv)
 	return 0;
 }
 
-bool run_main()
+static Uint32 main_callback(Uint32 interval, void *data)
 {
-	if (SDL_Init(SDL_INIT_AUDIO | SDL_INIT_VIDEO) != 0) {
-		return false;
+	SDL_Event event;
+	SDL_UserEvent userevent;
+
+	userevent.type = SDL_USEREVENT;
+	userevent.code = 0;
+	event.type = SDL_USEREVENT;
+	event.user = userevent;
+
+	SDL_PushEvent(&event);
+
+    return interval;
+}
+
+static bool run_main()
+{
+	if (SDL_Init(SDL_INIT_AUDIO | SDL_INIT_TIMER | SDL_INIT_VIDEO) != 0) {
+		throw Error("SDL_Init failed");
 	}
 
 	init_audio();
@@ -35,51 +51,73 @@ bool run_main()
 	init_font();
 	init_graphics();
 
-	Map *map = new Map("test.map");
+	map = new Map("test.map");
 
 	Player_Brain *player_brain = new Player_Brain();
-
-	Map_Entity *player = new Map_Entity(player_brain);
+	player = new Map_Entity(player_brain);
 	player->load_animation_set("sprites/player");
 	player->set_map(map);
 	player->set_position(Point<int>(1, 3));
-
 	map->add_entity(player);
 
-	// FIXME!!!!!!!!!! TIMER
+	SDL_AddTimer(16, main_callback, NULL);
 
-	while (1) {
-		SDL_Event event;
-		if (SDL_PollEvent(&event)) {
-			if (event.type == SDL_QUIT) {
+	bool quit = false;
+	bool draw = false;
+
+	while (quit == false) {
+		bool got_event = false;
+		while (true) {
+			SDL_Event event;
+			if (SDL_PollEvent(&event)) {
+				got_event = true;
+			}
+			else {
 				break;
+			}
+			if (event.type == SDL_QUIT) {
+				quit = true;
+				break;
+			}
+			else if (event.type == SDL_USEREVENT) {
+				map->update();
+				draw = true;
 			}
 			map->handle_event(&event);
 		}
 
-		map->update();
+		if (quit) {
+			break;
+		}
 
-		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
+		if (draw) {
+			draw = false;
 
-		map->draw();
+			glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+			glClear(GL_COLOR_BUFFER_BIT);
 
-		draw_window(5, screen_h/2, screen_w-10, screen_h/2-5);
+			map->draw();
 
-		font->draw("I've been wandering around this room for three days... there", 14, screen_h/2+9, four_whites[0]);
-		font->draw("are no windows or doors, just bright blue walls. All I can do", 14, screen_h/2+9+14, four_whites[0]);
-		font->draw("is dance. And I hate dancing. When I get out of here... IF I", 14, screen_h/2+9+28, four_whites[0]);
-		font->draw("get out of here...", 14, screen_h/2+9+42, four_whites[0]);
+			draw_window(5, screen_h/2, screen_w-10, screen_h/2-5);
 
-		flip();
+			font->draw("I've been wandering around this room for three days... there", 14, screen_h/2+9, four_whites[0]);
+			font->draw("are no windows or doors, just bright blue walls. All I can do", 14, screen_h/2+9+14, four_whites[0]);
+			font->draw("is dance. And I hate dancing. When I get out of here... IF I", 14, screen_h/2+9+28, four_whites[0]);
+			font->draw("get out of here...", 14, screen_h/2+9+42, four_whites[0]);
+
+			flip();
+		}
+		else if (!got_event) {
+			SDL_Delay(1);
+		}
 	}
 
 	delete map;
 
-	shutdown_audio();
-	shutdown_font();
 	shutdown_graphics();
+	shutdown_font();
 	shutdown_video();
+	shutdown_audio();
 	
 	return true;
 }
