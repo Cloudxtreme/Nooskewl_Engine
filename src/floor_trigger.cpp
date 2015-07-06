8834819b0c992fff261641506ac4726d0085f52c
Trent Gamblin <trent@nooskewl.ca>
2015-07-06 16:31:56 -0600

    Test map changing

--- src/floor_trigger.cpp
+++ src/floor_trigger.cpp
@@ -10,9 +10,12 @@ Floor_Trigger::Floor_Trigger(Point<int> topleft, Size<int> size, void (*function
 
 void ft_test(Map *map, Map_Entity *entity)
 {
+	/*
 	entity->stop();
 	std::vector<std::string> speeches;
 	speeches.push_back("YO! TESTING!");
 	speeches.push_back("We have received your goods and services tax/harmonized sales tax (GST/HST) return for the above-mentioned account for the period from January 1, 2011 to December 31, 2011. Because our records show that you already have a GST/HST return on file for this period, we did not process this duplicate record.");
 	map->add_speeches(speeches);
+	*/
+	map->change_map("test2.map", Point<int>(5, 5), N);
 }
