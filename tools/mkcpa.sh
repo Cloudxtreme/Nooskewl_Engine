afd2f40c477788d9b259cf46d9740ddaf656d57e
Trent Gamblin <trent@nooskewl.ca>
2015-07-07 00:19:01 -0600

    Add mkcpa.sh

---
+++ tools/mkcpa.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# This version runs on Windows using MSYS tools. You may need to change the
+# following path... Windows has a built-in find.exe that doesn't work with
+# this script.
+
+FILES=`/c/mingw/msys/1.0/bin/find . -type f | sort`
+
+echo "Writing header..."
+# the big space is a tab
+du -bc $FILES | grep "	total$" | cut -f1 > $1
+
+echo "Writing data..."
+cat $FILES >> $1
+
+echo "Writing info..."
+# sed removed "./" from beginning of filenames
+du -b $FILES | sed -e 's|./||' >> $1
+
+echo "Saving uncompressed archive..."
+cp $1 $1.uncompressed
+
+echo "Compressing..."
+gzip $1
+mv $1.gz $1
