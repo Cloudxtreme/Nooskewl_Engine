68ce3ff9abf43e5cb5ea8cd92154723064b14787
Trent Gamblin <trent@nooskewl.ca>
2015-07-08 21:48:04 -0600

    Add header including everything

---
+++ include/Nooskewl_Engine/Nooskewl_Engine.h
@@ -0,0 +1,33 @@
+#ifndef NOOSKEWL_ENGINE_H
+#define NOOSKEWL_ENGINE_H
+
+#include "Nooskewl_Engine/audio.h"
+#include "Nooskewl_Engine/brain.h"
+#include "Nooskewl_Engine/cpa.h"
+#include "Nooskewl_Engine/dllist.h"
+#include "Nooskewl_Engine/engine.h"
+#include "Nooskewl_Engine/error.h"
+#include "Nooskewl_Engine/font.h"
+#include "Nooskewl_Engine/graphics.h"
+#include "Nooskewl_Engine/image.h"
+#include "Nooskewl_Engine/load_dll.h"
+#include "Nooskewl_Engine/log.h"
+#include "Nooskewl_Engine/main.h"
+#include "Nooskewl_Engine/map.h"
+#include "Nooskewl_Engine/map_entity.h"
+#include "Nooskewl_Engine/map_logic.h"
+#include "Nooskewl_Engine/player_brain.h"
+#include "Nooskewl_Engine/resource_manager.h"
+#include "Nooskewl_Engine/snprintf.h"
+#include "Nooskewl_Engine/speech.h"
+#include "Nooskewl_Engine/sprite.h"
+#include "Nooskewl_Engine/tilemap.h"
+#include "Nooskewl_Engine/tween.h"
+#include "Nooskewl_Engine/types.h"
+#include "Nooskewl_Engine/util.h"
+#include "Nooskewl_Engine/vertex_accel.h"
+#include "Nooskewl_Engine/video.h"
+#include "Nooskewl_Engine/widgets.h"
+#include "Nooskewl_Engine/xml.h"
+
+#endif // NOOSKEWL_ENGINE_H
