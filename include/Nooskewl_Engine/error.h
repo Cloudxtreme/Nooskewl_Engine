82d544646555a49452251d77d1a8a0e6025143ad
Trent Gamblin <trent@nooskewl.ca>
2015-07-10 16:32:03 -0600

    Move other Error types into error.h

--- include/Nooskewl_Engine/error.h
+++ include/Nooskewl_Engine/error.h
@@ -12,6 +12,34 @@ public:
 	Error(std::string error_message) : error_message(error_message) {}
 };
 
+class NOOSKEWL_ENGINE_EXPORT MemoryError : public Error {
+public:
+	MemoryError(std::string error_message) {
+		this->error_message = "Memory error: " + error_message;
+	}
+};
+
+class NOOSKEWL_ENGINE_EXPORT LoadError : public Error {
+public:
+	LoadError(std::string error_message) {
+		this->error_message = "Load error: " + error_message;
+	}
+};
+
+class NOOSKEWL_ENGINE_EXPORT FileNotFoundError : public Error {
+public:
+	FileNotFoundError(std::string error_message) {
+		this->error_message = "File not found: " + error_message;
+	}
+};
+
+class NOOSKEWL_ENGINE_EXPORT GLError : public Error {
+public:
+	GLError(std::string error_message) {
+		this->error_message = "OpenGL error: " + error_message;
+	}
+};
+
 } // End namespace Nooskewl_Engine
 
 #endif // EXCEPTIONS_H
