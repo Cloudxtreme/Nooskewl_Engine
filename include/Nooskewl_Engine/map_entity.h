90de37a72720224a79650159c8e684855f31af26
Trent Gamblin <trent@nooskewl.ca>
2015-07-10 00:54:20 -0600

    Added is_solid/set_solid

--- include/Nooskewl_Engine/map_entity.h
+++ include/Nooskewl_Engine/map_entity.h
@@ -13,24 +13,27 @@ public:
 	Map_Entity(Brain *brain);
 	~Map_Entity();
 
+	void stop();
+	void handle_event(TGUI_Event *event);
+	// return false to destroy
+	bool update(Map *map);
+	void draw(Point<int> draw_pos);
+
 	void load_sprite(std::string name) throw (Error);
 	void set_position(Point<int> position);
 	void set_offset(Point<float> offset);
 	void set_bounce(int bounce);
 	void set_direction(Direction direction);
+	void set_solid(bool solid);
 
 	int get_id();
 	Direction get_direction();
 	Point<int> get_position();
 	Size<int> get_size();
 	Point<int> get_draw_position();
+	bool is_solid();
+	// returns true if they overlap even if not solid so check is_solid()
 	bool collides(Point<int> position, Size<int> size, Point<int> &collide_pos);
-
-	void stop();
-	void handle_event(TGUI_Event *event);
-	// return false to destroy
-	bool update(Map *map);
-	void draw(Point<int> draw_pos);
 
 private:
 	bool maybe_move(Map *map);
@@ -44,6 +47,7 @@ private:
 	float speed;
 	Point<float> offset;
 	int bounce;
+	bool solid;
 };
 
 #endif // MAP_ENTITY_H
