79cb1707d465cf7e4425a0019f01d3440ce0cc0f
Trent Gamblin <trent@nooskewl.ca>
2015-08-20 20:54:31 -0600

    Set the default value for substitute_yellow in default shader explicitly

--- src/Nooskewl_Engine/engine.cpp
+++ src/Nooskewl_Engine/engine.cpp
@@ -1511,6 +1511,7 @@ void Engine::setup_default_shader()
 {
 	current_shader = default_shader;
 	current_shader->use();
+	current_shader->set_bool("substitute_yellow", false);
 	current_shader->set_float("global_alpha", 1.0f);
 	current_shader->set_bool("drawing_text", false);
 	current_shader->set_bool("drawing_text_shadow", false);
