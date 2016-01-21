1a7fc526a10af789cd5425a38ccce1be5cfe6a1f
Trent Gamblin <trent@nooskewl.ca>
2016-01-21 07:17:24 -0700

    Add a Vec3D class

--- include/Nooskewl_Engine/basic_types.h
+++ include/Nooskewl_Engine/basic_types.h
@@ -425,6 +425,193 @@ public:
 	}
 };
 
+template<typename T> class Vec3D
+	{
+public:
+	T x;
+	T y;
+	T z;
+
+	Vec3D(T x, T y, T z)
+	{
+		this->x = x;
+		this->y = y;
+		this->z = z;
+	}
+
+	Vec3D(const Vec3D<int> &s)
+	{
+		this->x = (T)s.x;
+		this->y = (T)s.y;
+		this->z = (T)s.z;
+	}
+
+	Vec3D(const Vec3D<float> &s)
+	{
+		this->x = (T)s.x;
+		this->y = (T)s.y;
+		this->z = (T)s.z;
+	}
+
+	Vec3D()
+	{
+		x = y = z = 0;
+	}
+
+	float length()
+	{
+		float dx = (float)x;
+		float dy = (float)y;
+		float dz = (float)z;
+		return sqrtf(dx*dx + dy*dy + dz*dz);
+	}
+
+	T volume()
+	{
+		return x * y * z;
+	}
+
+	void operator=(const Vec3D<T> &from)
+	{
+		x = from.x;
+		y = from.y;
+		z = from.z;
+	}
+
+	inline Vec3D<T> operator+(const T &rhs)
+	{
+		Vec3D<T> s;
+		s.x = this->x + rhs;
+		s.y = this->y + rhs;
+		s.z = this->z + rhs;
+		return s;
+	}
+
+	inline Vec3D<T> operator-(const T &rhs)
+	{
+		Vec3D<T> s;
+		s.x = this->x - rhs;
+		s.y = this->y - rhs;
+		s.z = this->z - rhs;
+		return s;
+	}
+
+	inline Vec3D<T> operator*(const T &rhs)
+	{
+		Vec3D<T> s;
+		s.x = this->x * rhs;
+		s.y = this->y * rhs;
+		s.z = this->z * rhs;
+		return s;
+	}
+
+	inline Vec3D<T> operator/(const T &rhs)
+	{
+		Vec3D<T> s;
+		s.x = this->x / rhs;
+		s.y = this->y / rhs;
+		s.z = this->z / rhs;
+		return s;
+	}
+
+	inline Vec3D<T> &operator+=(const T &rhs)
+	{
+		this->x += rhs;
+		this->y += rhs;
+		this->z += rhs;
+		return *this;
+	}
+
+	inline Vec3D<T> &operator-=(const T &rhs)
+	{
+		this->x -= rhs;
+		this->y -= rhs;
+		this->z -= rhs;
+		return *this;
+	}
+
+	inline Vec3D<T> &operator*=(const T &rhs)
+	{
+		this->x *= rhs;
+		this->y *= rhs;
+		this->z *= rhs;
+		return *this;
+	}
+
+	inline Vec3D<T> &operator/=(const T &rhs)
+	{
+		this->x /= rhs;
+		this->y /= rhs;
+		this->z /= rhs;
+		return *this;
+	}
+
+	inline Vec3D<T> &operator-=(const Vec3D<T> &rhs)
+	{
+		this->x -= rhs.x;
+		this->y -= rhs.y;
+		this->z -= rhs.z;
+		return *this;
+	}
+
+	inline Vec3D<T> &operator+=(const Vec3D<T> &rhs)
+	{
+		this->x += rhs.x;
+		this->y += rhs.y;
+		this->z += rhs.z;
+		return *this;
+	}
+
+	inline Vec3D<T> &operator/=(const Vec3D<T> &rhs)
+	{
+		this->x /= rhs.x;
+		this->y /= rhs.y;
+		this->z /= rhs.z;
+		return *this;
+	}
+
+	inline Vec3D<T> &operator*=(const Vec3D<T> &rhs)
+	{
+		this->x *= rhs.x;
+		this->y *= rhs.y;
+		this->z *= rhs.z;
+		return *this;
+	}
+
+	inline Vec3D<T> operator-(const Vec3D<T> &rhs)
+	{
+		Vec3D<T> s(this->x - rhs.x, this->y - rhs.y, this->z - rhs.z);
+		return s;
+	}
+
+	inline Vec3D<T> operator+(const Vec3D<T> &rhs)
+	{
+		Vec3D<T> s(this->x + rhs.x, this->x + rhs.y, this->z + rhs.z);
+		return s;
+	}
+
+	inline Vec3D<T> operator/(const Vec3D<T> &rhs)
+	{
+		Vec3D<T> s(this->x / rhs.x, this->x / rhs.y, this->z / rhs.z);
+		return s;
+	}
+
+	inline Vec3D<T> operator*(const Vec3D<T> &rhs)
+	{
+		Vec3D<T> s(this->x * rhs.x, this->x * rhs.y, this->z * rhs.z);
+		return s;
+	}
+
+	inline Vec3D<T> operator-()
+	{
+		Vec3D<T> s;
+		s.x = -this->x;
+		s.y = -this->y;
+		s.z = -this->z;
+		return s;
+	}
+};
+
 } // End namespace Nooskewl_Engine
 
 #endif // TYPES_H
